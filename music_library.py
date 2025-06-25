b5e0dc2a3ca3881958026f6693a962dd1679886c
Akshat Parate <akshatparate@gmail.com>
2025-06-25 18:26:17 +0530

    Create music_library.py

---
+++ music_library.py
@@ -0,0 +1,13 @@
+music = {
+    "Natural": "https://www.youtube.com/watch?v=EjZJdw9gJ9g&list=PLqDyiQp0DOxCZ7GFMcKgTN5E_4NrLTCxi&index=2&pp=iAQB",
+
+    "Festival": "https://www.youtube.com/watch?v=vodU3w9jTVU&list=PLqDyiQp0DOxCZ7GFMcKgTN5E_4NrLTCxi&index=4&pp=iAQB",
+
+    "Serious": "https://www.youtube.com/watch?v=VaJ2I2rC4qU&list=PLqDyiQp0DOxCZ7GFMcKgTN5E_4NrLTCxi&index=42&pp=iAQB",
+
+    "Loco": "https://www.youtube.com/watch?v=BJ-nYWEpNVU&list=PLqDyiQp0DOxCZ7GFMcKgTN5E_4NrLTCxi&index=43&pp=iAQB",
+
+    "Universal": "https://www.youtube.com/watch?v=b56V-KyT9XM&list=PLqDyiQp0DOxCZ7GFMcKgTN5E_4NrLTCxi&index=54&pp=iAQB",
+
+    "Happy": "https://www.youtube.com/watch?v=AH1VgE4nl38&list=PLqDyiQp0DOxCZ7GFMcKgTN5E_4NrLTCxi&index=66&pp=iAQB"
+}
